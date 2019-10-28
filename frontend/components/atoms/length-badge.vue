ec47b362b43501f098b0b3bd0cdde9b62ed17aab
nomuken <nomuken@spica.bz>
2019-10-28 15:48:19 +0900

    :sparkles: composition-style queue

---
+++ frontend/components/atoms/length-badge.vue
@@ -0,0 +1,34 @@
+<template>
+  <span v-bind:class="{ working: isActive }">{{ len }}</span>
+</template>
+
+<script lang="ts">
+import { computed, createComponent, PropType } from "@vue/composition-api"
+export default createComponent({
+  name: "LengthBadge",
+  props: {
+    elements: {
+      type: Array as PropType<any[]>,
+      required: true,
+    },
+  },
+  setup({ elements }) {
+    const len = computed(() => {
+      return elements.length
+    })
+    const isActive = computed(() => {
+      return elements.length !== 0
+    })
+
+    return {
+      len,
+      isActive,
+    }
+  },
+})
+</script>
+
+<style lang="stylus" scoped>
+.working
+  color #0f0
+</style>
--- frontend/components/menu/queue/Queue.vue
+++ frontend/components/menu/queue/Queue.vue
@@ -5,77 +5,33 @@
         :to="{ name: 'jobs' }"
         :active="current_length !== 0 || jobs.queued.length !== 0"
       >
-        Processing: <span :class="current_class">{{ current_length }}</span> /
-        Queued:
-        <span :class="queued_class">{{ jobs.queued.length }}</span>
+        Processing:
+        <length-badge :elements="[jobs.current_job]"></length-badge> / Queued:
+        <length-badge elements="jobs.queued_jobs"></length-badge>
       </b-nav-item>
     </b-navbar-nav>
   </div>
 </template>
+
 <script lang="ts">
-import { mapState, mapMutations } from "vuex"
-import { computed, createComponent, reactive } from "@vue/composition-api"
+import { createComponent, onMounted, PropType } from "@vue/composition-api"
+import LengthBadge from "@/components/atoms/length-badge.vue"
 
 export default createComponent({
   name: "Queue",
+  components: {
+    LengthBadge,
+  },
+  props: {
+    jobs: {
+      type: Object as PropType<Jobs>,
+      required: true,
+    },
+  },
   setup() {
-    // computed
-    const current_length = computed(() => this.jobs.current !== null ? 1 : 0)
-  },
-  data() {
-    return {
-      paused: false,
-    }
-  },
-  mounted() {
-    this.next_tick()
-  },
-  computed: {
-    current_length() {
-      return this.jobs.current !== null ? 1 : 0
-    },
-    current_class() {
-      return this.current_length === 0 ? null : ["working"]
-    },
-    queued_class() {
-      return this.jobs.queued.length === 0 ? null : ["working"]
-    },
-    ...mapState(["jobs"]),
-  },
-  methods: {
-    next_tick() {
-      this.fetch_jobs()
-      if (!this.paused) {
-        if (this.jobs.current !== null) {
-          setTimeout(this.next_tick, 5000)
-        } else {
-          setTimeout(this.next_tick, 10000)
-        }
-      }
-    },
-    fetch_jobs() {
-      this.$axios
-        .get("/jobs", { progress: false })
-        .then(res => {
-          if (res.data.status_code === 0) {
-            this.change_current(res.data.current_job)
-            this.push_queued_jobs(res.data.queued_jobs)
-          }
-        })
-        .catch(e => {
-          console.error(`Fetching jobs caused a error: ${e}`)
-          this.paused = true
-        })
-    },
-    ...mapMutations({
-      change_current: "jobs/change_current",
-      push_queued_jobs: "jobs/push_queued_jobs",
-    }),
+    onMounted(() => {
+      this.$accessor.registerFetchJobsWorker()
+    })
   },
 })
 </script>
-
-<style lang="stylus" scoped>
-.working
-  color #0f0
-</style>
